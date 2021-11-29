1b50b7fe00ae5f34744abdc6b33af29330173e4d
Melek Lassoued <65103260+meleklassoued@users.noreply.github.com>
2021-11-29 11:27:00 +0100

    success with nextjs and styled components

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,38 @@
-import styled from "styled-components";
+import Head from "next/head";
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+const GlobalStyle = createGlobalStyle`
+html{
+  box-sizing: border-box;
+  background: #F5F4F0;
+  display:block;
+  height: 100%;
+  max-width: 640px;
+  margin:0 auto;
+  padding: 0;
+}
 
+body{
+  background-color:#fafafa;
+  min-height:100vh;
+  padding: 1rem;
+  margin-top:0;
+  font-family:Verdana;
+}
+
+`;
+const theme = {
+  colors: {
+    primary: "#fafafa",
+  },
+};
 export default function Home() {
-  const Title = styled.h1`
-    font-size: 1.5em;
-    text-align: center;
-    color: red;
-  `;
-  return <Title>aslema</Title>;
+  return (
+    <div>
+      <Head>
+        <title>NextJS Blog with Butter CMS</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <h1> NextJS Blog with Butter CMS</h1>
+    </div>
+  );
 }
