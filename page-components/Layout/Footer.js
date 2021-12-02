91f7489af0a4029d7c2499931deada8f4fb13e90
Melek Lassoued <65103260+meleklassoued@users.noreply.github.com>
2021-12-02 19:32:43 +0100

    change the footer background color on click

--- page-components/Layout/Footer.js
+++ page-components/Layout/Footer.js
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useContext } from "react";
 import styles from "../../style/Layout.module.scss";
+import { ThemeContext } from "../Layout";
+
 function Footer() {
-  return (
-    <footer className={styles.pagefooter}>
-      <p>
-        &copy;{new Date().getfullYear} <span>Github Users</span>. built By{" "}
-        <a href='https://github.com/meleklassoued'>Melek lassoued</a>
-      </p>
-    </footer>
-  );
+  const { Theme } = useContext(ThemeContext);
+  if (Theme === false) {
+    return (
+      <footer className={styles.pagefooter}>
+        <p>
+          &copy;{new Date().getfullYear} <span>Github Users</span>. built By{" "}
+          <a href='https://github.com/meleklassoued'>Melek lassoued</a>
+        </p>
+      </footer>
+    );
+  } else {
+    return (
+      <footer className={styles.pagefooterwhite}>
+        <p>
+          &copy;{new Date().getfullYear} <span>Github Users</span>. built By{" "}
+          <a href='https://github.com/meleklassoued'>Melek lassoued</a>
+        </p>
+      </footer>
+    );
+  }
 }
 
 export default Footer;
