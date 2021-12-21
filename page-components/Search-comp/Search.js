edfb47f2b47796ce795d8d2c36c68f19cead5d58
Melek Lassoued <65103260+meleklassoued@users.noreply.github.com>
2021-12-21 14:37:48 +0100

    make the search functions and prepare thecomponent

--- page-components/Search-comp/Search.js
+++ page-components/Search-comp/Search.js
@@ -1,18 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import Styles from "../Search-comp/Search.module.scss";
 
 function Search() {
+  const [State, setState] = useState({
+    search: "malik",
+  });
+  const { search } = State;
+
+  const handleForm = (e) => {
+    setState({
+      search: e.target.value,
+    });
+  };
+
+  const SearchUser = (e) => {
+    e.prevent.default();
+    props.getUserSearch(search);
+  };
+
+  console.log(search);
   return (
     <div className={Styles.container}>
-      <form action='get'>
+      <form onSubmit={SearchUser}>
         <label className={Styles.label} htmlFor='Users'>
           search a User
         </label>
-        <input className={Styles.input} type='text' name='Users' /> <br />
-        <button className={Styles.button} type='submit'>
-          {" "}
-          click
-        </button>
+        <input
+          placeholder='Search user...'
+          className={Styles.input}
+          type='text'
+          name='Users'
+          onChange={handleForm}
+          value={search}
+        />{" "}
+        <br />
+        <button className={Styles.button}> click</button>
       </form>
     </div>
   );
