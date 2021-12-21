168bb82792ccbbdf20a6903b1d51186eff5940a6
Melek Lassoued <65103260+meleklassoued@users.noreply.github.com>
2021-12-21 14:38:42 +0100

    add search component to the GithubUserS component

--- page-components/GithubUsers/index.js
+++ page-components/GithubUsers/index.js
@@ -1,15 +1,19 @@
 import React from "react";
 import User from "../GithubUsers/User";
+import SearchUsers from "../Search-comp/Search";
 import styles from "../../page-components/GithubUsers/User.module.scss";
 
 function Index({ data }) {
   return (
-    <div className={styles.container}>
-      {data.map((user) => (
-        // eslint-disable-next-line react/jsx-key
-        <User user={user} />
-      ))}
-    </div>
+    <>
+      <SearchUsers />
+      <div className={styles.container}>
+        {data.map((user) => (
+          // eslint-disable-next-line react/jsx-key
+          <User user={user} />
+        ))}
+      </div>
+    </>
   );
 }
 
